c46d626fb0ab9d4b9ed8a89c6b90a9f91f96fdc2
kengaku <keng9358@gmail.com>
2020-06-08 13:47:31 +0900

    グローバル汚染対策

--- module-sample/func2.js
+++ module-sample/func2.js
@@ -1,8 +1,10 @@
-var Hoge = function () { }
-Hoge.prototype.add = function (val1, val2) {
-    return val1 + val2
-}
-Hoge.prototype.sub = function (val1, val2) {
-    return val1 - val2
-}
-module.exports = new Hoge
+(function () {
+    var Hoge = function () { }
+    Hoge.prototype.add = function (val1, val2) {
+        return val1 + val2
+    }
+    Hoge.prototype.sub = function (val1, val2) {
+        return val1 - val2
+    }
+    module.exports = new Hoge
+})()
