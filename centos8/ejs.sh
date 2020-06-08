34a3f207bb88ff9abe7113ed013e2c217952c295
kengaku <keng9358@gmail.com>
2020-06-08 16:04:11 +0900

    ejs 環境追加

--- centos8/ejs.sh
+++ centos8/ejs.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-dir="/git/node-trainning/ejs-sample1"
-[ -d ${dir} ] || mkdir -p ${dir}
-cd ${dir}
-yarn init -y
-yarn add ejs
+
+main(){
+    dir=${1}
+    [ -d ${dir} ] || mkdir -p ${dir}
+    cd ${dir}
+    yarn init -y
+    yarn add ejs
+}
+
+main "/git/node-trainning/ejs-sample1"
+main "/git/node-trainning/ejs-sample2"
