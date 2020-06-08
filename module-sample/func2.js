b87cdb45e199031f29257876f062a96631846c7c
kengaku <keng9358@gmail.com>
2020-06-08 13:39:31 +0900

    function.prototype の module.exports

---
+++ module-sample/func2.js
@@ -0,0 +1,8 @@
+var Hoge = function () { }
+Hoge.prototype.add = function (val1, val2) {
+    return val1 + val2
+}
+Hoge.prototype.sub = function (val1, val2) {
+    return val1 - val2
+}
+module.exports = new Hoge
---
+++ module-sample/sample2.js
@@ -0,0 +1,3 @@
+var func2 = require('./func2')
+console.log(func2.add(2, 5))
+console.log(func2.sub(7, 11))
